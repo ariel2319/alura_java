7a17ae592e301d92a1db0374427ad922aca6422a
ariel2319 <maldonadoaa.91@gmail.com>
2024-05-04 17:00:26 -0300

    variables de referencia..

--- alura_listas_y_colecciones_de_datos/src/com/alura/screenmatch/principal/PrincipalConListas.java
+++ alura_listas_y_colecciones_de_datos/src/com/alura/screenmatch/principal/PrincipalConListas.java
@@ -10,11 +10,13 @@ public class PrincipalConListas {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula("Encanto", 2021);
         miPelicula.evalua(8);
-        Pelicula otraPelicula = new Pelicula("Avatar", 2013);
+        Pelicula otraPelicula = new Pelicula("Encanto", 2021);
         otraPelicula.evalua(9);
         var peliculaDeBruno = new Pelicula("El señor de los Anillos", 2001);
         peliculaDeBruno.evalua(10);
         Serie lost = new Serie("Lost", 2000);
+
+        Pelicula p1 = miPelicula;
 
         ArrayList<Titulo> lista = new ArrayList<>();
         lista.add(miPelicula);
@@ -22,11 +24,27 @@ public class PrincipalConListas {
         lista.add(peliculaDeBruno);
         lista.add(lost);
 
-        //mostrar "toString" de cada serie y película
+        // mostrar "toString" de cada serie y película
 
         for (Titulo item : lista) {
-            System.out.println(item);
-            System.out.println(item.g);
+            System.out.println(item.getNombre());
+            //desde JAVA 14 puedo usar variables de referencia...
+            if (item instanceof Pelicula pelicula && pelicula.getClasificacion()>4) {
+                System.out.println(pelicula.getClasificacion());
+            }
+            //otra forma de hacer lo mismo de arriba...
+            // if (item instanceof Pelicula) {
+            //     Pelicula pelicula = (Pelicula) item;
+            //     System.out.println(pelicula.getClasificacion());
+            // }
+        }
+
+        System.out.println("P1" + p1);
+
+        if (miPelicula == otraPelicula) {
+            System.out.println("son iguales");
+        } else {
+            System.out.println("distintas");
         }
 
 
